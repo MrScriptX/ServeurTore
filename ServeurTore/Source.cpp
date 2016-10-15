d1ef7046d57e8f0e536aea11c28d1f9e1349a053
liebault <liebault@SEBASTIEN>
2016-10-15 18:46:13 +0200

    CoreServer 1.1

--- ServeurTore/Source.cpp
+++ ServeurTore/Source.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include "serveur.h"
 
 int main()
 {
+	int broadCast{ 0 };
+	
+	std::cin >> broadCast;
+
+	if (broadCast == 0)
+	{
+		Server localServ(1111);//local server
+
+		for (int i = 0; i < 100; i++)
+		{
+			localServ.ListenNewConnections();
+		}
+	}
+	else if (broadCast == 1)
+	{
+		Server publicServ(1111, true);//public server
+
+		for (int i = 0; i < 100; i++)
+		{
+			publicServ.ListenNewConnections();
+		}
+	}
+
+	system("pause");
 	return 0;
 }
